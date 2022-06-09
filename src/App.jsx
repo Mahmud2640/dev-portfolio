6e767be5cd1390575f5e89c0d23cc2855199569f
Mahmud2640 <mdmahmud484@gmail.com>
2022-06-09 22:07:34 +0600

    added projects

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,17 @@ import Contact from "./Components/Contact/Contact";
 import Experience from "./Components/Experience/Experience";
 import Footer from "./Components/Footer/Footer";
 import Header from "./Components/Header/Header";
+import Navbar from "./Components/Navbar/Navbar";
+import Portfolio from "./Components/Portfolio/Portfolio";
 
 function App() {
   return (
     <>
       <Header />
+      <Navbar />
       <About />
       <Experience />
+      <Portfolio />
       <Contact />
       <Footer />
     </>
