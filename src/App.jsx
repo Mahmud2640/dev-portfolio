85c854f62feb7d0dd834308212e5b3e24273bf40
Mahmud2640 <mdmahmud484@gmail.com>
2022-06-10 01:15:48 +0600

    particles added

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Experience from "./Components/Experience/Experience";
 import Footer from "./Components/Footer/Footer";
 import Header from "./Components/Header/Header";
 import Navbar from "./Components/Navbar/Navbar";
-// import ParticlesBg from "./Components/Particles/ParticlesBg";
+import Particle from "./Components/Particles/Particle";
 import Portfolio from "./Components/Portfolio/Portfolio";
 import Reviews from "./Components/Reviews/Reviews";
 import Services from "./Components/Services/Services";
@@ -12,7 +12,7 @@ import Services from "./Components/Services/Services";
 function App() {
   return (
     <div>
-      {/* <ParticlesBg /> */}
+      <Particle />
       <Header />
       <Navbar />
       <About />
